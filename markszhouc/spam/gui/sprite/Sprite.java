dac60bbd08782207ad150635e533e622cbab60f7
MarkSturtevant <sturtevm@gmail.com>
2021-09-06 03:43:07 -0500

    Delete markszhouc/spam directory

--- markszhouc/spam/gui/sprite/Sprite.java
+++
@@ -1,8 +0,0 @@
-package markszhouc.spam.gui.sprite;
-
-public abstract class Sprite {
-
-    public abstract void render(double mouseX, double mouseY, int frameSpeed);
-    public abstract void testClick(double mouseX, double mouseY);
-
-}
